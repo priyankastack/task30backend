5a5eec66e69d1b7097ddf639a0fb4892c3286aaf
Priyanka singh <160533231+PRIYANKA22BSC@users.noreply.github.com>
2025-03-29 13:31:49 +0530

    commited

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express();
 const cors=require("cors");
 
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://task-30-lovat.vercel.app",
     methods:"GET,POST,PATCH,PUT,DELETE",
     credentials:true
 }
